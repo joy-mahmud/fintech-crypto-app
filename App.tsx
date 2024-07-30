a38ef31651f4a3ad4f3bd2386c82e27851093fc6
joy-mahmud <joymahmudiucse@gmail.com>
2024-07-30 16:46:17 +0600

    basic folder structure setup

---
+++ App.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const App = () => {
+  return (
+    <View className='flex-1 justify-center items-center bg-neutral-800'>
+      <Text className='text-white font-bold'>Hello world</Text>
+    </View>
+  )
+}
+
+export default App
