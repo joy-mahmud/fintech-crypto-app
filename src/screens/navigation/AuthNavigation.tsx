8ea1fe81f79ee89b219d33534bebd847ee96bd7e
joy-mahmud <joymahmudiucse@gmail.com>
2024-07-31 00:22:00 +0600

    auth navigation basic part done

--- src/screens/navigation/AuthNavigation.tsx
+++ src/screens/navigation/AuthNavigation.tsx
@@ -1,11 +1,25 @@
 import { View, Text } from 'react-native'
 import React from 'react'
-
+import { createStackNavigator,TransitionPresets } from '@react-navigation/stack'
+import SplashScreen from '../auth/SplashScreen'
+import WelcomeScreen from '../auth/WelcomeScreen'
+import LoginScreen from '../auth/LoginScreen'
+import RegisterScreen from '../auth/RegisterScreen'
+const Stack = createStackNavigator()
 const AuthNavigation = () => {
   return (
-    <View>
-      <Text>AuthNavigation</Text>
-    </View>
+    <Stack.Navigator screenOptions={{
+        headerShown:false,
+        ...TransitionPresets.SlideFromRightIOS,
+        animationEnabled:true,
+        gestureEnabled:true,
+        gestureDirection:'horizontal'
+        }}>
+        <Stack.Screen name='splash' component={SplashScreen}/>
+        <Stack.Screen name='welcome' component={WelcomeScreen}/>
+        <Stack.Screen name='login' component={LoginScreen}/>
+        <Stack.Screen name='register' component={RegisterScreen}/>
+    </Stack.Navigator>
   )
 }
 
--- src/screens/navigation/RootNavigation.tsx
+++ src/screens/navigation/RootNavigation.tsx
@@ -1,26 +1,34 @@
 import { View, Text } from 'react-native'
 import React, { useState } from 'react'
-import {createStackNavigator,TransitionPresets} from '@react-navigation/stack'
+import { createStackNavigator, TransitionPresets } from '@react-navigation/stack'
 import { NavigationContainer } from '@react-navigation/native'
 import TabNavigation from './tabNavigation'
 import AuthNavigation from './AuthNavigation'
 
 const RootNavigation = () => {
-    const [session,setSession]=useState(true)
+    const [session, setSession] = useState(false)
     const Stack = createStackNavigator()
-  return (
-    <NavigationContainer>
-        <Stack.Navigator>
-            {
-            session?(
-                <Stack.Screen name='TabNavigation' component={TabNavigation}/>
-            ):(
-                <Stack.Screen name='AuthNavigation' component={AuthNavigation}/>
-            )
-            }
+    return (
+        <NavigationContainer>
+            <Stack.Navigator screenOptions={{
+                headerShown: false,
+                ...TransitionPresets.SlideFromRightIOS,
+                animationEnabled: true,
+                gestureEnabled: true,
+                gestureDirection: 'horizontal'
+            }}
+
+            >
+                {
+                    session ? (
+                        <Stack.Screen name='TabNavigation' component={TabNavigation} />
+                    ) : (
+                        <Stack.Screen name='AuthNavigation' component={AuthNavigation} />
+                    )
+                }
             </Stack.Navigator>
-    </NavigationContainer>
-  )
+        </NavigationContainer>
+    )
 }
 
 export default RootNavigation
